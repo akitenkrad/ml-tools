0344bbbed6b64f0a9766c0d984f0dca3009c7a12
akitenkrad <akitenkrad@gmail.com>
2023-11-12 18:50:56 +0900

    initial commit

---
+++ ml_tools/utils/logger.py
@@ -0,0 +1,130 @@
+import logging
+from datetime import datetime, timedelta, timezone
+from logging import Formatter, Logger, StreamHandler, getLogger
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+
+def get_json_liner(name: str, logfile: str = "") -> Logger:
+    """Generate Logger instance
+
+    Args:
+        name: (str) name of the logger
+        logfile: (str) logfile name
+    Returns:
+        Logger
+    """
+
+    # --------------------------------
+    # 0. mkdir
+    # --------------------------------
+    if logfile == "":
+        JST = timezone(timedelta(hours=+9), "JST")
+        now = datetime.now(JST)
+        now = datetime(now.year, now.month, now.day, now.hour, now.minute, 0, tzinfo=JST)
+        log_dir = Path("./logs") / now.strftime("%Y%m%d%H%M%S")
+        log_dir.mkdir(parents=True, exist_ok=True)
+        logfile = name
+    else:
+        log_dir = Path(logfile).parent
+        log_dir.mkdir(parents=True, exist_ok=True)
+        logfile = Path(logfile).name
+
+    # --------------------------------
+    # 1. logger configuration
+    # --------------------------------
+    logger = getLogger(name)
+    for h in logger.handlers:
+        logger.removeHandler(h)
+    logger.setLevel(logging.DEBUG)
+
+    if logger.hasHandlers():
+        handler_format = Formatter("")
+        # --------------------------------
+        # 3. log file configuration
+        # --------------------------------
+        fh = RotatingFileHandler(str(log_dir / name), maxBytes=2**30, backupCount=3000)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(handler_format)
+        logger.addHandler(fh)
+
+        # --------------------------------
+        # 4. error log file configuration
+        # --------------------------------
+        er_fh = RotatingFileHandler(str(log_dir / name), maxBytes=2**30, backupCount=3000)
+        er_fh.setLevel(logging.ERROR)
+        er_fh.setFormatter(handler_format)
+        logger.addHandler(er_fh)
+
+    return logger
+
+
+def get_logger(name, logfile: str = "", silent: bool = False) -> Logger:
+    """Generate Logger instance
+
+    Args:
+        name (str): name of the logger
+        logfile (str): logfile name
+        silent (bool): if True, not log into stream
+    Returns:
+        Logger
+    """
+
+    # --------------------------------
+    # 0. mkdir
+    # --------------------------------
+    if logfile == "":
+        JST = timezone(timedelta(hours=+9), "JST")
+        now = datetime.now(JST)
+        now = datetime(now.year, now.month, now.day, now.hour, now.minute, 0, tzinfo=JST)
+        log_dir = Path("./logs") / now.strftime("%Y%m%d%H%M%S")
+        log_dir.mkdir(parents=True, exist_ok=True)
+        logfile = name
+    else:
+        log_dir = Path(logfile).parent
+        log_dir.mkdir(parents=True, exist_ok=True)
+        logfile = Path(logfile).name
+
+    # --------------------------------
+    # 1. logger configuration
+    # --------------------------------
+    logger = getLogger(name)
+    for h in logger.handlers:
+        logger.removeHandler(h)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.hasHandlers():
+        handler_format = Formatter("%(asctime)s [%(levelname)8s] %(name)15s - %(message)s")
+
+        # --------------------------------
+        # 2. handler configuration
+        # --------------------------------
+        if not silent:
+            stream_handler = StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            stream_handler.setFormatter(handler_format)
+            logger.addHandler(stream_handler)
+
+        # --------------------------------
+        # 3. log file configuration
+        # --------------------------------
+        fh = RotatingFileHandler(str(log_dir / logfile), maxBytes=3145728, backupCount=3000)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(handler_format)
+        logger.addHandler(fh)
+
+        # --------------------------------
+        # 4. error log file configuration
+        # --------------------------------
+        er_fh = RotatingFileHandler(str(log_dir / logfile), maxBytes=3145728, backupCount=3000)
+        er_fh.setLevel(logging.ERROR)
+        er_fh.setFormatter(handler_format)
+        logger.addHandler(er_fh)
+
+    return logger
+
+
+def kill_logger(logger: Logger):
+    name = logger.name
+    if name in logging.Logger.manager.loggerDict:
+        del logging.Logger.manager.loggerDict[name]
---
+++ setup.py
@@ -0,0 +1,72 @@
+import sys
+
+import setuptools
+
+with open("README.md", mode="r") as f:
+    long_description = f.read()
+
+version_range_max = max(sys.version_info[1], 10) + 1
+python_min_version = (3, 10, 0)
+
+setuptools.setup(
+    name="ml_tools",
+    version="0.0.1",
+    author="akitenkrad",
+    author_email="akitenkrad@gmail.com",
+    packages=setuptools.find_packages(),
+    package_data={
+        "ml_tools": [
+            "config/*.yml",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+    + ["Programming Language :: Python :: 3.{}".format(i) for i in range(python_min_version[1], version_range_max)],
+    long_description=long_description,
+    install_requires=[
+        "attrdict @ git+https://github.com/akitenkrad/attrdict",
+        "beautifulsoup4",
+        "click",
+        "colorama",
+        "gensim",
+        "ipython",
+        "ipywidgets",
+        "mlflow",
+        "nltk",
+        "numpy",
+        "pandas",
+        "plotly",
+        "progressbar",
+        "py-cpuinfo",
+        "python-dateutil",
+        "python-dotenv",
+        "pyunpack",
+        "requests",
+        "scikit-learn",
+        "scipy",
+        "seaborn",
+        "transformers",
+        "toml",
+        "torch",
+        "torchinfo",
+        "torchtext",
+        "torchvision",
+        "tqdm",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "mypy",
+            "flake8",
+            "isort",
+            "jupyterlab",
+            "types-python-dateutil",
+            "types-PyYAML",
+            "types-requests",
+            "typing-extensions",
+            "types-toml",
+        ]
+    },
+)
